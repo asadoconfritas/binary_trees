5ab0b4d5c42daca4471098ecdf0e1fe6716b447b
root <guzchelsea24@gmail.com>
2022-03-25 19:59:34 -0700

    last com

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,10 +1,31 @@
 #include "binary_trees.h"
+
 /**
- *
- *
- *
+ * binary_tree_is_leaf - check if a node is a leaf
+ * @node: ptr to the node
+ * Return: 1 if true, 0 if false
  **/
-p
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	a
+	if (node == NULL)
+		return (0);
+	if (node->left != NULL || node->right != NULL)
+		return (0);
+	else
+		return (1);
+}
+
+/**
+ * binary_tree_leaves - measures the height of a binary tree
+ * @tree: ptr to the root
+ * Return: end program
+ **/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t l = 0;
+
+	if (!tree)
+		return (0);
+	l = binary_tree_is_leaf(tree);
+	return (l + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,35 @@
 #include "binary_trees.h"
+
 /**
- *
- *
- *
+ * binary_tree_is_leaf - check if a node is a leaf
+ * @node: ptr to the node
+ * Return: 1 if true, 0 if false
  **/
-p
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	a
+	if (node == NULL)
+		return (0);
+	if (node->left != NULL || node->right != NULL)
+		return (0);
+	else
+		return (1);
+}
+
+/**
+ * binary_tree_nodes - measures the height of a binary tree
+ * @tree: ptr to the root
+ * Return: end program
+ **/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t lflag = 0, cf = 0;
+
+	if (!tree)
+		return (0);
+	lflag = binary_tree_is_leaf(tree);
+	if (lflag == 1)
+		cf = 0;
+	else
+		cf = 1;
+	return (cf + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
